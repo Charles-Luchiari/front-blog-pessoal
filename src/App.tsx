2687d909e2168aa9813bfb966c174e0abcd248d8
Charles-Luchiari <charles99na@gmail.com>
2023-06-16 15:57:08 -0300

    refatorando

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import Navbar from './components/estaticos/navbar/Navbar';
-import Footer from './components/estaticos/footer/footer';
+import Footer from './components/estaticos/footer/Footer';
 import './App.css';
 import Home from './paginas/home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './paginas/login/Login';
 
 
 function App() {
 
   return (
-    <>
-      <Navbar />
-       <Home />
+    <BrowserRouter>
+      {/* Tudo que precisa ser renderizado na tela que vai direcionar para algum lugar fica aqui */}
+        <Navbar />
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+      </Routes>
       <Footer />
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
 export default App;
